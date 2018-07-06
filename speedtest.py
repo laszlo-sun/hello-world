a20e4412eac140417dd05b3259ecb69ee980c515
laszloSun <laszlo_sun@yahoo.com>
2018-07-06 11:28:38 +0900

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -1864,7 +1864,7 @@ def shell():
         results.share()
 
     if args.simple:
-        printer('%s,%0.2f M%s/s' %,
+        printer('%sZ%0.2f M%s/s' %,
                 (datetime.datetime.utcnow().isoformat(),
                     (results.download / 1000.0 / 1000.0) / args.units[1],
                  args.units[0]))
