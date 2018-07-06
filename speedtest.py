8319cda4c28508aad9e4a594e52f5bbde9c5e557
laszloSun <laszlo_sun@yahoo.com>
2018-07-06 11:20:19 +0900

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -1866,9 +1866,8 @@ def shell():
     if args.simple:
         printer('%0.2f M%s/s,%sZ' %,
                 ((results.download / 1000.0 / 1000.0) / args.units[1],
-                 args.units[0],
-                datetime.datetime.utcnow().isoformat(),
-                args.units[0]))
+                 datetime.datetime.utcnow().isoformat(),
+                 args.units[0]))
     elif args.csv:
         printer(results.csv(delimiter=args.csv_delimiter))
     elif args.json:
